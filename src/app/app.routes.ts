29ecb19027e710db91468311086cc1ccf32e6013
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-07-28 17:25:02 -0300

    prueba de routing en sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,6 +17,10 @@ export const routes: Routes = [
         component: ContactPageComponent
     },
     {
+        path: 'countries',
+        loadChildren: () => import('./countries/countries.module').then( m => m.CountriesModule)
+    },
+    {
         path: '**',
         redirectTo: 'home'
     }
