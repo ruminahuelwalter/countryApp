dcdf1238624db7c0a3679e41c65849c2ee8f6dc4
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-07-29 17:08:36 -0300

    countryTable

---
+++ src/app/countries/components/country-table/country-table.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Country } from 'app/countries/interfaces/country';
+
+@Component({
+  selector: 'countries-table',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './country-table.component.html',
+  styles: `img{
+    width: 25px;
+  }`
+})
+export class CountryTableComponent {
+  @Input()
+  public countries: Country[] = [];
+
+}
--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -3,14 +3,16 @@ import { SharedModule } from 'app/shared/shared.module';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from 'app/countries/interfaces/country';
 import { CommonModule } from '@angular/common';
+import { CountryTableComponent } from "../../components/country-table/country-table.component";
 
 @Component({
   selector: 'app-by-capital-page',
   standalone: true,
   imports: [
     CommonModule,
+    CountryTableComponent,
     SharedModule
-  ],
+],
   templateUrl: './by-capital-page.component.html',
   styles: ``
 })
