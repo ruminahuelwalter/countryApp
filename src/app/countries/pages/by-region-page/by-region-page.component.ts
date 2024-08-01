4f29aecf76de9ee1390fac06a352bc8ec832dad9
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-08-01 18:55:32 -0300

    busqueda por region

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -5,6 +5,7 @@ import { Country } from 'app/countries/interfaces/country';
 import { CountryTableComponent } from "../../components/country-table/country-table.component";
 import { CommonModule } from '@angular/common';
 
+type Region = 'Africa'|'Americas'| 'Asia'|'Europe'|'Oceania';
 
 @Component({
   selector: 'app-by-region-page',
@@ -18,13 +19,18 @@ import { CommonModule } from '@angular/common';
   styles: ``
 })
 export class ByRegionPageComponent {
-  public countries: Country[] = []
+  public regions: Region[] = ['Africa', 'Americas','Asia','Europe','Oceania'];
+  public countries: Country[] = [];
+  public selectedRegion?: Region; 
+
   public isLoading: boolean = false;
   constructor( private countriesService: CountriesService ){}
- 
-  searchByRegion( term: string): void {
+
+  searchByRegion( region: Region): void {
+
+    this.selectedRegion = region;
     this.isLoading = true
-    this.countriesService.searchRegion( term ).
+    this.countriesService.searchRegion( region ).
     subscribe(
       countries => {
         this.countries = countries;
